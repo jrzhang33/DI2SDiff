728657c0bb387b7c329d639f42251f80127b87cd
Junru Zhang <136706139+jrzhang33@users.noreply.github.com>
2024-07-04 16:11:12 +0800

    Update get_domainhar.py

--- data_load/get_domainhar.py
+++ data_load/get_domainhar.py
@@ -6,46 +6,7 @@ from data_load.data_util.get_dataloader import load
 
 import data_load.utils
 import os
-# def args_parse():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--num_workers', type=int, default=1)
-#     parser.add_argument('--seed', type=int, default=1)
-#     parser.add_argument('--dataset', type=str, default='dsads')
-#     parser.add_argument('--target', type=int, default=0)
-#     parser.add_argument('--n_act_class', type=int, default=19,
-#                         help='the number of the category of activities')
-#     parser.add_argument('--n_aug_class', type=int,
-#                         default=8, help='including the ori one')
-#     parser.add_argument('--auglossweight', type=float, default=1)
-#     parser.add_argument('--conweight', type=float, default=1.0)
-#     parser.add_argument('--dp', type=str, default='dis',
-#                         help='this is for oirginal and aug feature discrimination')
-#     parser.add_argument('--dpweight', type=float, default=10.0,
-#                         help='this is the weight of dp')
-#     parser.add_argument('--n_feature', type=int, default=64)
-#     parser.add_argument('--lr', type=float, default=0.0008)
-#     parser.add_argument('--n_epoch', type=int, default=500)
-#     parser.add_argument('--batch_size', type=int, default=128)
-#     parser.add_argument('--early_stop', type=int, default=20)
-#     parser.add_argument('--epoch_based_training', type=str2bool, default=False,
-#                         help="Epoch-based training / Iteration-based training")
-#     parser.add_argument('--n_iter_per_epoch', type=int,
-#                         default=200, help="Used in Iteration-based training")
-#     parser.add_argument('--remain_data_rate', type=float, default=1.0,
-#                         help='The percentage of data used for training after reducing training data.')
-#     parser.add_argument('--scaler_method', type=str, default='norm') #minmax
-#     parser.add_argument('--root_path', type=str,
-#                         default="/home/ddlearn/data/")
-#     parser.add_argument('--data_save_path', type=str,
-#                         default="/home/ddlearn/data/")
-#     parser.add_argument('--save_path', type=str,
-#                         default="/home/results/")
 
-#     args = parser.parse_args()
-#     args.step_per_epoch = 100000000000
-#     args = param_init(args)
-#     print_environ()
-#     return args
 
 
 
