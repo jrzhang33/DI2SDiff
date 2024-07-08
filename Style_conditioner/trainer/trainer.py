810dd6d78de9d268b05ea153efa13102deac900c
Junru Zhang <136706139+jrzhang33@users.noreply.github.com>
2024-07-08 19:56:12 +0800

    Update trainer.py

--- Style_conditioner/trainer/trainer.py
+++ Style_conditioner/trainer/trainer.py
@@ -13,64 +13,7 @@ from datetime import datetime
 
 time = datetime.now().strftime("%Y%m%d_%H%M%S")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-def get_fc(x):
-    x = x.squeeze(2).unsqueeze(3)
-    b, c, d , e= x.size()
-    x_fft, x_fft_init=transfft(x)
-    k1=torch.Tensor(np.arange(1,x_fft.shape[2]+1)).to(device).repeat(x_fft.shape[0],x_fft.shape[1],1)
-    fc=k1.unsqueeze(3)*x_fft
-    y_1=(torch.sum(fc,dim=2)/torch.sum(x_fft,dim=2)).view(b,c,1,1) #fc
-    return y_1.squeeze(3).squeeze(2)
 
-def get_low(x):
-    x = x.squeeze(2).unsqueeze(3)
-    b, c, d , e= x.size()
-    x_fft, x_fft_init=transfft(x)
-    k1=torch.Tensor(np.arange(1,x_fft.shape[2]+1)).to(device).repeat(x_fft.shape[0],x_fft.shape[1],1)
-    fc=k1.unsqueeze(3)*x_fft
-    y_1=(torch.sum(fc,dim=2)/torch.sum(x_fft,dim=2)).view(b,c,1,1) #fc
-    fc1=y_1.to(torch.int)
-    x_= x_fft_init
-    for i in range(x_.shape[0]):
-        for j in range(x_.shape[1]):
-            s = fc1[i,j,0,0].item()
-           # s = int(x_.shape[2]/2)
-            x_[i,j,s:]=0
-    x2=torch.fft.irfft(x_,n=d,dim=2)
-    return x2
-
-def get_high(x):
-    x = x.unsqueeze(3)
-    b, c, d , e= x.size()
-    x_fft, x_fft_init=transfft(x)
-    k1=torch.Tensor(np.arange(1,x_fft.shape[2]+1)).to(device).repeat(x_fft.shape[0],x_fft.shape[1],1)
-    fc=k1.unsqueeze(3)*x_fft
-    y_1=(torch.sum(fc,dim=2)/torch.sum(x_fft,dim=2)).view(b,c,1,1) #fc
-    fc1=y_1.to(torch.int)
-    x_= x_fft_init
-    for i in range(x_.shape[0]):
-        for j in range(x_.shape[1]):
-            s = fc1[i,j,0,0].item()
-            #s = int(x_.shape[2]/2)
-            x_[i,j,:s]=0
-    x2=torch.fft.irfft(x_,n=d,dim=2)
-    return x2
-
-
-def transfft(x_input):
-    x_fft_init = torch.fft.rfft(x_input[..., 0],dim=2,norm="forward")
-    x_fft = torch.stack((x_fft_init.real, x_fft_init.imag), -1)
-    x_fft = x_fft.detach().cpu().numpy()
-
-    for i in range(x_fft.shape[0]): 
-        if i==0:
-            ff = np.sqrt(x_fft[i,:,:,0]**2 + x_fft[i,:,:,1]**2) 
-            ff=ff.reshape(1,x_fft.shape[1],x_fft.shape[2],1)
-            continue 
-        f = np.sqrt(x_fft[i,:,:,0]**2 + x_fft[i,:,:,1]**2).reshape(1,x_fft.shape[1],x_fft.shape[2],1)
-        ff=np.concatenate([ff,f],0)
-    x_fft=torch.from_numpy(ff[:,:,:-1,:]).to(device)
-    return x_fft,x_fft_init
 
 
 
